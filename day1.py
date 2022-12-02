6f97292b15a2f2e20fb3ba5ce71d152e6f1faf54
cooperhancock <coophan11@gmail.com>
2022-12-01 22:56:49 -0600

    day 1

--- day1.py
+++ day1.py
@@ -1,4 +1,20 @@
 input = []
-INPUTFILE = "..\\2.txt"
+INPUTFILE = "in1.txt"
 with open(INPUTFILE, 'r') as f:
     input = f.readlines()
+
+localCount = 0
+carryList = []
+for n in input:
+    if n == '' or n == '\n':
+        carryList.append(localCount)
+        localCount = 0
+    else:
+        localCount += int(n)
+
+print(max(carryList))
+print(sum(sorted(carryList, reverse=True)[:3]))
+
+
+
+
